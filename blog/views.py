295c8d5ffaf3d3fae215faeab29505b4d31d5c25
Maria Azrar <maria.azrar@arbisoft.com>
2024-08-15 12:11:33 +0500

    added SEO friendly URLs

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+
+# from django.http import Http404
 # Create your views here.
 
 def post_list(request):
@@ -9,6 +10,11 @@ def post_list(request):
 
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, 
+                             slug=post, 
+                             publish__year=year, 
+                             publish__month=month, 
+                             publish__day=day, 
+                             status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
