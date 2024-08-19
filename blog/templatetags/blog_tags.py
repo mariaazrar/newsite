a39cad8b688d0cb293866e122089a94232c6d092
Maria Azrar <maria.azrar@arbisoft.com>
2024-08-19 16:55:40 +0500

    added queryset tag and filter to the project

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 from ..models import Post
 
 register = template.Library()
@@ -11,3 +14,12 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    commented_posts = Post.published.annotate(most_commented=Count('comments')).order_by('-most_commented')[:count]
+    return commented_posts
+
+@register.filter(name='markdown')
+def markdown_format(body):
+    return mark_safe(markdown.markdown(body))
