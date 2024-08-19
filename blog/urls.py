c8d8c4ec7a3db5bab39ea54aca1140e45fa2f3e8
Maria Azrar <maria.azrar@arbisoft.com>
2024-08-19 20:24:29 +0500

    Added RSS Feed to the application

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import post_list, post_detail, share_post, comment_post
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
     path('<int:post_id>/share/', share_post, name='post_share'),
-    path('<int:post_id>/comment/', comment_post, name='post_comment')
+    path('<int:post_id>/comment/', comment_post, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed')
 ]
